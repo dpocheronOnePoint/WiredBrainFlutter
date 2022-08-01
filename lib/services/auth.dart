9d1d704819458b1031b3cd05674d4f22497ffc56
your_username <your_email@gmail.com>
2022-08-01 09:24:36 +0200

    Add Auth service

---
+++ lib/services/auth.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  AuthService._();
+
+  static final AuthService _service = AuthService._();
+  factory AuthService() => _service;
+
+  static AuthService get instance => _service;
+
+  Stream<User?> authStateChange() => _firebaseAuth.authStateChanges();
+
+  User? get currentUser => _firebaseAuth.currentUser;
+}
